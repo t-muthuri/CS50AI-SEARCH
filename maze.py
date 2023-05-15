034035e2acb2c562e8c8e71ad739b536fb049ff8
t-muthuri <tressiemuthuri@gmail.com>
2023-05-15 16:46:52 +0300

    updated solution working code

--- maze.py
+++ maze.py
@@ -140,7 +140,7 @@ class Maze():
         candidate = list of movements from the current state.
         """
         candidates = [
-            ("up", (row -1, col)),
+            ("up", (row - 1, col)),
             ("down", (row + 1, col)),
             ("left", (row, col - 1)),
             ("right", (row, col + 1))
@@ -150,7 +150,7 @@ class Maze():
         for action, (r, c) in candidates:
             if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
                 result.append((action, (r,c)))
-            return result
+        return result
         
     def solve(self):
         """
